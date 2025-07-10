21490eaca00940564671d6d8d294eec5f83be293
Kingsley Loo-Xu <kingsleyloolay@outlook.com>
2025-07-09 21:11:30 -0700

    Created custom dataset class, started working on training code

---
+++ dataset.py
@@ -0,0 +1,36 @@
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import os 
+
+class ParkingDataset(Dataset):
+    def __init__(self, img_dir, label_dir, transform=None):
+        self.img_dir = img_dir
+        self.label_dir = label_dir
+        self.transform = transform
+        self.images = sorted(os.listdir(img_dir))
+        self.labels = sorted(os.listdir(label_dir))
+
+    
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+
+        img_path = os.path.join(self.img_dir, self.images[idx])
+        label_path = os.path.join(self.label_dir, self.labels[idx])
+
+        #open and resize images 
+        img= Image.open(img_path)
+        if self.transform:
+            img = self.transform(img)
+        
+        #convert labels to tensors 
+        labels = []
+        with open(label_path, 'r') as x:
+            for line in x:
+                parts = list(map(float, line.strip().split()))
+                labels.append(parts)
+        labels = torch.tensor(labels, dtype=torch.float32)
+
+        return img, labels
