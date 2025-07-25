3217718810ec1b6692ee8e64d511e2b80083e947
Kingsley Loo-Xu <kingsleyloolay@outlook.com>
2025-07-24 22:22:15 -0700

    model trained

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 import torch
 
 
-model = YOLO('model_best.pt')
+model = YOLO('model.pt')
 
 # test images are currently the same as validation images, will add dedicated testing images once more data is created 
 # conf = minimum confidence for model to classify an object 
-results = model.predict(source="data/images/val", save=True, conf=0.1)
+results = model.predict(source="data/images/val", save=True, conf=0.25)
